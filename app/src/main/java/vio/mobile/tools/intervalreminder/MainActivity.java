90959e84d594c4d3b81652489e6ad4fe78a4e9cb
sandraloewig <sandra.schreiner@gmail.com>
2022-02-24 18:43:04 +0100

    First Notification try

--- app/src/main/java/vio/mobile/tools/intervalreminder/MainActivity.java
+++ app/src/main/java/vio/mobile/tools/intervalreminder/MainActivity.java
@@ -1,14 +1,45 @@
 package vio.mobile.tools.intervalreminder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean blinkOn = false;
+    private static final String CHANNEL_ID  = "IntervalReminder";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Timer tmrBlink = new Timer(5000, new Runnable() {
+
+            public void run() {
+               Toast.makeText(MainActivity.this, "BLINK!", Toast.LENGTH_SHORT).show();
+
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this)
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentTitle("Reminder")
+                        .setContentText("Bitte trinken")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
+
+                int notificationId = (int) System.currentTimeMillis();
+// notificationId is a unique int for each notification that you must define
+                notificationManager.notify(notificationId, builder.build());
+            }
+
+        });
+
+        tmrBlink.start();
+
     }
 }
